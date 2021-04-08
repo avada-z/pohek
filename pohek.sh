ba6f800def4750d6127101a1f2bf9ae0a182d193
avada-z <82169276+avada-z@users.noreply.github.com>
2021-04-08 20:09:54 +0200

    hashcat mode

--- pohek.sh
+++ pohek.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/bash
 args=($@)
 rm out.txt >> /dev/null
-if ! which ffuf > /dev/null || ! which jq > /dev/null || ! which nmap > /dev/null; then
-   echo -e "some packages not found! Install? (y/n) \c"
+if ! which ffuf > /dev/null || ! which jq > /dev/null || ! which nmap > /dev/null || ! which hashcat > /dev/null || ! which hashcat-nvidia > /dev/null; then
+   echo -e "Some packages not found! Install? (y/n) \c"
    read
    if [ "$REPLY" = "y" ] || [ "$REPLY" = "Y" ] 
 then
-      sudo apt install ffuf jq nmap -y >> /dev/null && exit 0
+      sudo apt install ffuf jq nmap hashcat hashcat-nvidia -y >> /dev/null && exit 0
 elif [ "$REPLY" = "n" ] || [ "$REPLY" = "" ]
 	then
 	exit 0
@@ -14,7 +14,7 @@ elif [ "$REPLY" = "n" ] || [ "$REPLY" = "" ]
 fi
 if [[ $1 = "--help" ]] || [[ $1 = "-h" ]] || [[ $1 = "" ]]
 then
-echo "pohek (URL without https) (Path to the ffuf wordlist) or --i or --install for installing to /usr/bin" && exit 1
+echo "pohek (URL without https) (Path to the ffuf wordlist) or --i or --install for installing to /usr/bin; --hashcat for the hashcat mode (hashcat hash id) (path for hash) (path for wordlist)" && exit 0
 elif [[ $1 = "-i" ]] || [[ $1 = "--install" ]]
 then
 if which /usr/bin/pohek > /dev/null
@@ -23,13 +23,18 @@ echo "pohek is installed!" && exit 1
 fi
 sudo cp pohek.* /usr/bin/pohek && sudo chmod 777 /usr/bin/pohek && exit 0
 fi
-nmap -O -sV $1 >> out1.txt
+if [[ $1 = "--hashcat" ]]
+then
+echo "hashcat mode!"
+hashcat -m $2 -a 0 --quiet -o out-hash.txt -w 4 -O $3 $4
+fi
+nmap -O -sV $1 > out1.txt
 echo "nmap done"
 ffuf -u https://$1/FUZZ -w $2 -o $1.json $1.html -of json html -sf true -mc 200,403 >> /dev/null
 echo "ffuf done"
-for output in $(jq -r ".results[].input[]" $1.json);do echo $output >> dirdar-$1.txt ;done
+for output in $(jq -r ".results[].input[]" $1.json);do echo $output > dirdar-$1.txt ;done
 echo "ffuf output filtered"
-dirdar -only-ok -wl dirdar-$1.txt -single https://$1/ >> out3.txt
+dirdar -only-ok -wl dirdar-$1.txt -single https://$1/ > out3.txt
 echo "dirdar done"
-cat out1.txt out2.txt out3.txt >> out.txt
+cat out1.txt out2.txt out3.txt out-hash.txt 1> out.txt
 rm out1.txt out2.txt out3.txt dirdar-$1.txt
